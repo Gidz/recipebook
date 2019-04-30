ad165270d9f36f6f3896f1bbc2c386693a07dd8c
Gideon Paul <gidzpaul95@gmail.com>
2019-04-30 12:22:23 -0400

    Recipe belongs to an User.

--- recipes/models.py
+++ recipes/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 #TODO: Add User field, Step field later
 class Recipe(models.Model):
     name = models.CharField(max_length=100, null=False)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
