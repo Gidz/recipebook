0c176444f56af4f2ffb32e035133c169b8bec3e4
Gideon Paul <gidzpaul95@gmail.com>
2019-04-30 12:08:13 -0400

    Extended django's AbstractUser and added email, first and last name fields.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser 
 
-# Create your models here.
+class User(AbstractUser):
+    """
+    Extending the default User model to add more fields.
+    """
+    first_name = models.CharField(max_length=30) 
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length = 50, unique=True)
+
