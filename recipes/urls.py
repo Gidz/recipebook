ea2d7c8b2ddf097ef26e882741fa5aa6f5b2b9eb
Gideon Paul <gidzpaul95@gmail.com>
2019-04-30 23:12:48 -0400

    Retrieve objects by username.

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from django.urls import include
 from rest_framework import routers
 
-from .views import RecipeViewSet
+from .views import RecipeViewSet, RecipesByUser
 
 router = routers.DefaultRouter()
 router.register('recipes',RecipeViewSet)
 
 urlpatterns = [
-    path('api/',include(router.urls))
+    path('api/', include(router.urls)),
+    path('api/<slug:user>/recipes/', RecipesByUser.as_view()),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,21 @@
 from .models import Recipe
 from .serializers import RecipeSerializer
-
+from rest_framework.response import Response
 from rest_framework import viewsets
+from rest_framework import views
 
-#class RecipeView(generics.RetrieveUpdateDestroyAPIView):
-#    queryset = Recipe.objects.all()
-#    serializer_class = RecipeSerializer
-#
-#    @classmethod
-#    def get_extra_actions(cls):
-#        return []
+
+class RecipesByUser(views.APIView):
+     def get(self, request, format=None, **kwargs):
+
+        username = self.kwargs['user']
+
+        recipe_objects = Recipe.objects.filter(user__username=username)
+
+        recipes = [ recipe.name for recipe in recipe_objects]
+        serializer = RecipeSerializer(recipes, many=True)
+        return Response(recipes)
+
 
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
